ebcbc3b9b502be8bd95e50f086c19926397e488f
vovoka <vovoka.path@gmail.com>
2022-08-04 17:24:16 +0300

    feat: set TS, Webpack, ESlint, Prettier, Babel ('npm start' works!)

---
+++ client/webpack.config.js
@@ -0,0 +1,69 @@
+// webpack.config.js
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
+
+const baseConfig = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.ts'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: '[name].bundle.js',
+        assetModuleFilename: 'assets/[name][ext]',
+    },
+    mode: 'development',
+    module: {
+        rules: [
+            {
+                test: /\.ts$/i,
+                use: 'ts-loader',
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
+            // Images
+            {
+                test: /\.(?:ico|gif|png|jpe?g|svg)$/i,
+                // use: 'file-loader',
+                type: 'asset/resource',
+                generator: {
+                    filename: 'assets/images/[hash][ext]',
+                },
+            },
+            // fonts, SVG
+            {
+                test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+                type: 'asset/inline',
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, './src/index.html'),
+            filename: 'index.html',
+        }),
+        new CleanWebpackPlugin(),
+        new EslingPlugin({ extensions: 'ts' }),
+    ],
+    resolve: {
+        extensions: ['.ts', '.js'],
+    },
+};
+
+module.exports = ({ mode }) => {
+    const isProductionMode = mode === 'prod';
+    const envConfig = isProductionMode ? require('./webpack.prod.config') : require('./webpack.dev.config');
+
+    return merge(baseConfig, envConfig);
+};
