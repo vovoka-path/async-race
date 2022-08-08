3d3fd0e978e611f96ca7b7db18ae51913e9b8d7e
vovoka <vovoka.path@gmail.com>
2022-08-08 22:16:08 +0300

    feat: create types

--- client/src/components/types/types.ts
+++ client/src/components/types/types.ts
@@ -1,13 +1,65 @@
-import Button from "../view/button/button";
-import Form from "../view/form/form";
+import Button from '../view/button/button';
+import Form from '../view/form/form';
 
-export interface MethodList {
-    [key: string]: Function;
+export interface CarsData {
+    count: string | null;
+    data: CarData[];
 }
 
-export interface Data {
-    [key: string]: string;
+export interface CarData {
+    name: string;
+    color: string;
+    id?: number;
 }
+
+export interface Paths {
+    garage: string;
+    winners: string;
+    engine: string;
+}
+
+export type DataFunction = (data: CarData) => void;
+export type NameFunction = (id: string) => void;
+// export type NameFunction = (name: string | ObserverFunction | CBfunc, id?: string) => void;
+export type GetFunction = () => CarData;
+// export type TrackFunction = (buttonName: string, id: string) => void;
+export type ObserverFunction = (name?: CarData | string | number) => void | Promise<void> | null;
+export type CBfunc = (currenrPage: number) => Promise<void> | null;
+export type CallbaclFunction = (id: string | number | CarData) => void;
+export type FormName = 'create' | 'update';
+export type ButtonName = 'race' | 'reset' | 'generate';
+export type ElementName = FormName | ButtonName;
+
+export interface InputMethodList {
+    [key: string]: DataFunction;
+}
+
+export interface SubmitMethodList {
+    [key: string]: NameFunction | CallbaclFunction; // | TrackFunction;
+}
+
+export interface GetMethodList {
+    [key: string]: GetFunction;
+}
+
+// export interface SubmitMethodList1 {
+//     [key: string]: string;
+// }
+
+export interface FormsData {
+    create: CarData;
+    update: CarData;
+}
+
+// export interface FormData {
+//     name: string;
+//     color: string;
+// }
+
+// export interface FormsData {
+//     create: FormData;
+//     update: FormData;
+// }
 
 export type TypesOfElement = typeof Button | typeof Form;
 // export type SubmitElement = Button | Form;
@@ -15,8 +67,15 @@ export interface Elements {
     [key: string]: Button | Form;
 }
 
-// export interface ElementClass {
-//     [key: string]: string;
-// }
+export interface FormsState {
+    createText: string;
+    createColor: string;
+}
+
+export interface VendorCars {
+    Make: string;
+    Model: string;
+    [key: string]: string;
+}
 
 // export type ElementClass = typeof Button | typeof Form;
