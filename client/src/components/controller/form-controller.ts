ccffd01525949d1810ecead8f49aaf1d3fcdc903
vovoka <vovoka.path@gmail.com>
2022-08-08 22:21:54 +0300

    feat: create Form controller

--- client/src/components/controller/form-controller.ts
+++ client/src/components/controller/form-controller.ts
@@ -1,58 +1,156 @@
 import Model from '../model/model';
 import Observer from './observer';
-import { MethodList, Data } from '../types/types';
+// import { CarData } from '../types/types';
+import { InputMethodList, GetMethodList, CarData } from '../types/types';
 
-const model = new Model();
-
-const inputMethods: MethodList = {
-    create: model.setInputCreate,
-    update: model.setInputUpdate,
-};
-
-const submitMethods: MethodList = {
-    create: model.createCar,
-    update: model.updateCar,
-};
-
-class ControllerForm extends Observer {
+class FormController extends Observer {
     name: string;
     model: Model;
-    modelInputMethod: Function;
-    modelSubmitMethod: Function;
+    id: string;
+    // modelInputMethod: DataFunction;
+    // modelSubmitMethod: DataFunction;
     // Coming: 'create' or 'update'
-    constructor(formName: string) {
+    constructor(formName: string, id?: string) {
         super();
         this.name = formName;
         this.model = new Model();
-        this.modelInputMethod = inputMethods[this.name];
-        this.modelSubmitMethod = submitMethods[this.name];
+        this.id = id ? id : '';
     }
 
-    input(data: Data) {
-        this.modelInputMethod(data);
-        // const method = methods[this.name];
-        // this.model[method](data);
-        // switch (this.formName) {
-        //     case 'create': this.model.setInputCreate(data);
-        //         break;
-        //     case 'update': this.model.setInputUpdate(data);
-        //         break;
-        // }
-        // console.log(this.formName, '### MyController.input = ', data)
+    input(data: CarData) {
+        const inputMethods: InputMethodList = {
+            create: (data) => this.model.setInputCreate(data),
+            update: (data) => this.model.setInputUpdate(data),
+        };
+
+        inputMethods[this.name](data);
+        // this.modelInputMethod(data);
     }
 
-    submit(data: Data) {
-        this.modelSubmitMethod(data);
-        // const method = methods[this.name];
-        // this.model[method](data);
-        // switch (this.formName) {
-        //     case 'create': this.model.createCar(data);
+    submit(data: CarData) {
+        const submitMethods: InputMethodList = {
+            create: (data) => this.model.createCar(data),
+            update: (data) => this.model.updateCar(data),
+        };
+
+        submitMethods[this.name](data);
+        // this.modelSubmitMethod(data);
+    }
+
+    getFormDataFromStorage() {
+        return this.model.formsData;
+    }
+
+    getFormData() {
+        const getMethods: GetMethodList = {
+            create: () => this.model.getInputCreate(),
+            update: () => this.model.getInputUpdate(),
+        };
+
+        getMethods[this.name]();
+        // let formData;
+
+        // switch (this.name) {
+        //     case 'create':
+        //         formData = this.model.getInputCreate();
         //         break;
-        //     case 'update': this.model.updateCar(data);
+        //     case 'update':
+        //         formData = this.model.getInputUpdate();
         //         break;
         // }
-        // console.log(this.formName, '### MyController.submit = ', data)
+
+        // return formData;
     }
 }
 
-export default ControllerForm;
+export default FormController;
+
+//=======================================
+
+// const inputMethods: InputMethodList = {
+//     create: (data) => this.model.setInputCreate(data),
+//     update: (data) => this.model.setInputUpdate(data),
+// };
+
+// this.modelInputMethod = inputMethods[this.name];
+
+// const submitMethods: InputMethodList = {
+//     create: (data) => this.model.createCar(data),
+//     update: (data) => this.model.updateCar(data),
+// };
+
+// this.modelSubmitMethod = submitMethods[this.name];
+
+// const model = new Model();
+
+// const inputMethods: MethodList = {
+//     create: model.setInputCreate as DataFunction,
+//     update: model.setInputUpdate as DataFunction,
+// };
+
+// const submitMethods: MethodList = {
+//     create: model.createCar,
+//     update: model.updateCar,
+// };
+
+// console.log('inputMethods[create]', inputMethods['create']);
+
+// this.modelInputMethod = this.assignInputMethodByName(formName);
+// this.modelSubmitMethod = this.assignSubmitMethodByName(formName);
+// console.log('this.modelInputMethod', this.modelInputMethod);
+// this.assignMethodsByName(this.name);
+
+// private assignInputMethodByName(formName: string) {
+//     // if (formName === 'test') return null;
+//     const inputMethods: MethodList = {
+//         create: this.model.setInputCreate as DataFunction,
+//         update: this.model.setInputUpdate as DataFunction,
+//     };
+
+//     // this.modelInputMethod = inputMethods[formName];
+//     console.log('formName', formName);
+//     console.log('assignMethodsByName this.modelInputMethod', this.modelInputMethod);
+//     return inputMethods[formName];
+// }
+
+// private assignSubmitMethodByName(formName: string) {
+//     const submitMethods: MethodList = {
+//         create: this.model.createCar,
+//         update: this.model.updateCar,
+//     };
+
+//     // console.log('formName', formName);
+//     // console.log('submitMethods[formName]', submitMethods[formName]);
+//     // this.modelSubmitMethod = submitMethods[formName];
+//     // console.log('this.modelSubmitMethod', this.modelSubmitMethod);
+//     return submitMethods[formName];
+// }
+
+// console.log('this.modelInputMethod', this.modelInputMethod);
+// (() => this.modelInputMethod(data))();
+
+// const method = methods[this.name];
+// this.model[method](data);
+// switch (this.name) {
+//     case 'create':
+//         this.model.setInputCreate(data);
+//         break;
+//     case 'update':
+//         this.model.setInputUpdate(data);
+//         break;
+// }
+// console.log(this.formName, '### MyController.input = ', data)
+
+// if (Math.ceil(this.model.carCount / 7) === this.model.getCarsPageFromLocalStorage()) {
+
+// }
+// // this.model[method](data);
+// switch (this.name) {
+//     case 'create':
+//         this.model.createCar(data);
+//         break;
+//     case 'update':
+//         this.model.updateCar(data);
+//         break;
+// }
+// console.log(this.formName, '### MyController.submit = ', data)
