476bc8f7a3b57ba9ef592f1aad814c24d4c305ed
vovoka <vovoka.path@gmail.com>
2022-08-08 22:20:45 +0300

    feat: create Winners

--- client/src/components/view/winners/winners.ts
+++ client/src/components/view/winners/winners.ts
@@ -1,3 +1,32 @@
-class Winners {}
+class Winners {
+    // parent: HTMLElement;
+    winners: HTMLDivElement;
+    constructor() {
+        // this.parent = parentElement;
+        const winners = document.createElement('div');
+        this.winners = winners;
+    }
+
+    render(parentElement: HTMLElement) {
+        // this.clear();
+        const test = this.getHTMLElement('div', 'winners');
+        test.innerText = 'Winners!';
+        this.winners.append(test);
+        // this.parent = parentElement;
+        parentElement.append(this.winners);
+    }
+
+    private getHTMLElement(name: string, styles?: string): HTMLElement {
+        const element = document.createElement(name);
+        element.className = `${styles}`;
+
+        return element;
+    }
+
+    clear() {
+        this.winners.innerHTML = '';
+        // this.winners.remove();
+    }
+}
 
 export default Winners;
