aa71770928a41979d3ba42c468134135e2f68b5e
vovoka <vovoka.path@gmail.com>
2022-08-25 17:53:00 +0300

    fix: update nav-controller

--- client/src/components/controller/nav-controller.ts
+++ client/src/components/controller/nav-controller.ts
@@ -1,17 +1,13 @@
 import model from './get-model';
-// import Model from '../model/model';
 import Observer from './observer';
 
 class NavController extends Observer {
-    // private pageName: string;
     private model = model;
     constructor() {
         super();
-        // this.pageName = this.model.getPage();
     }
 
     click(name: string) {
-        // this.pageName = name;
         this.model.setPageName(name);
     }
 
