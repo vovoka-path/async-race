66cb59336fdb00e800f403dd090ab81b25c27f6d
vovoka <vovoka.path@gmail.com>
2022-08-25 17:52:46 +0300

    fix: update get-model

--- client/src/components/controller/get-model.ts
+++ client/src/components/controller/get-model.ts
@@ -3,15 +3,3 @@ import Model from '../model/model';
 const model = new Model();
 
 export default model;
-
-// import Model from '../model/model';
-
-// class Controller {
-//     model = new Model();
-
-//     getModel() {
-//         return this.model;
-//     }
-// }
-
-// export default Controller;
