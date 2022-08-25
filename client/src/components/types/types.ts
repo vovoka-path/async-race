7c2a05061f4a04915c18325ded42186f6b2c4a8f
vovoka <vovoka.path@gmail.com>
2022-08-25 17:54:11 +0300

    fix: update types

--- client/src/components/types/types.ts
+++ client/src/components/types/types.ts
@@ -23,9 +23,7 @@ export interface Paths {
 
 export type DataFunction = (data: CarData) => void;
 export type NameFunction = (id: string) => void;
-// export type NameFunction = (name: string | ObserverFunction | CBfunc, id?: string) => void;
 export type GetFunction = () => CarData;
-// export type TrackFunction = (buttonName: string, id: string) => void;
 export type ObserverFunction = (name?: CarData | string | number) => void | Promise<void> | null;
 export type CBfunc = (currenrPage: number) => Promise<void> | null;
 export type CallbaclFunction = (id: string | number | CarData) => void;
@@ -38,34 +36,19 @@ export interface InputMethodList {
 }
 
 export interface SubmitMethodList {
-    [key: string]: NameFunction | CallbaclFunction; // | TrackFunction;
+    [key: string]: NameFunction | CallbaclFunction;
 }
 
 export interface GetMethodList {
     [key: string]: GetFunction;
 }
 
-// export interface SubmitMethodList1 {
-//     [key: string]: string;
-// }
-
 export interface FormsData {
     create: CarData;
     update: CarData;
 }
 
-// export interface FormData {
-//     name: string;
-//     color: string;
-// }
-
-// export interface FormsData {
-//     create: FormData;
-//     update: FormData;
-// }
-
 export type TypesOfElement = typeof Button | typeof Form;
-// export type SubmitElement = Button | Form;
 export interface Elements {
     [key: string]: Button | Form;
 }
@@ -81,8 +64,6 @@ export interface VendorCars {
     [key: string]: string;
 }
 
-// export type ElementClass = typeof Button | typeof Form;
-
 export interface HeaderData {
     elementName: string;
     titleText: string;
@@ -95,27 +76,15 @@ export interface NavMenuElements {
 }
 
 export type CbData = string | number | CarData | HTMLElement;
-// export type CallbackType<T> = ((pageName: T) => void) | (() => void);
 type CallbackType1<T> = (pageName: T) => void;
 type CallbackType2 = () => void;
 export type CallbackType3<T> = () => T;
 export type CallbackType<T> = CallbackType1<T> | CallbackType2 | CallbackType3<T>;
-// export type CallbackType = (pageName: string) => void | (() => void);
-// export type CallbackTypes = CallbackType[];
 
 export interface ViewElements {
     header: Header;
     main: Main;
-    // parent: HTMLElement;
 }
-
-// export interface FuncData {
-//     [key: string]: string | number;
-// }
-
-// export type NameOrId<T extends number | string > = T extends number
-// ? IdLabel
-// : NameLabel;
 
 export interface GarageElements {
     inputFormElements: Elements;
