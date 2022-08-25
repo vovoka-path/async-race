a1155c5707135b5d8ffc4932af62a6e73a27df01
vovoka <vovoka.path@gmail.com>
2022-08-25 17:53:12 +0300

    fix: update observer

--- client/src/components/controller/observer.ts
+++ client/src/components/controller/observer.ts
@@ -13,25 +13,12 @@ class Observer {
     unsubscribe<T>(fn: Partial<CallbackType<T>>): void {
         this.subscribers.filter((subscriber: Partial<CallbackType<T>>) => subscriber !== fn);
     }
-    //CarData | string | number | never // ObserverFunction | CBfunc |
+
     broadcast<T>(data: T): void {
-        // console.log('this.subscribers.length = ', this.subscribers.length);
         this.subscribers.forEach(async (subscriber: Partial<CallbackType<T>>) => {
             await (subscriber as CallableFunction)(data);
-            // console.log('%%% ------------------------');
-            // console.log('%%% data =', data, 'subscriber =', subscriber);
         });
     }
 }
 
 export default Observer;
-
-// if (typeof data === 'string') {
-//     subscriber(data as string);
-// } else if (typeof data === 'number') {
-//     subscriber(data as number);
-// } else if (typeof data === 'CarData') {
-//     subscriber(data as CarData);
-// } else {
-//     subscriber();
-// }
