6787499eda00216c38563fa7433b4b7167249075
vovoka <vovoka.path@gmail.com>
2022-08-25 17:56:04 +0300

    fix: update winners

--- client/src/components/view/winners/winners.ts
+++ client/src/components/view/winners/winners.ts
@@ -1,31 +1,21 @@
+import { createHTMLElement } from '../../utils/create-elements';
+
 class Winners {
-    // parent: HTMLElement;
     winners: HTMLDivElement;
     constructor() {
-        // this.parent = parentElement;
         const winners = document.createElement('div');
         this.winners = winners;
     }
 
     render(parentElement: HTMLElement) {
-        // this.clear();
-        const test = this.getHTMLElement('div', 'winners');
+        const test = createHTMLElement('div', 'winners');
         test.innerText = 'Winners!';
         this.winners.append(test);
-        // this.parent = parentElement;
         parentElement.append(this.winners);
-    }
-
-    private getHTMLElement(name: string, styles?: string): HTMLElement {
-        const element = document.createElement(name);
-        element.className = `${styles}`;
-
-        return element;
     }
 
     clear() {
         this.winners.innerHTML = '';
-        // this.winners.remove();
     }
 }
 
